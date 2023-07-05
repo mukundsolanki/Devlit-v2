14792745b868ccaa9f9299fee6d6f5136113b9a5
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-05 11:45:26 +0530

    Added routes and designed the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import FirebaseAuthCheck from './components/FirebaseAuthCheck';
+import StartBuilding from "./components/StartBuilding";
+import About from "./components/About";
+import Profile from "./components/Profile";
+
 
 function App() {
 
   return (
     <div className="App">
-      <FirebaseAuthCheck/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<FirebaseAuthCheck />} />
+          <Route path="/start-building" element={<StartBuilding />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,55 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Header from "./Header";
+import { auth } from "./config";
 
-function Home(){
+function Home() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
     return (
         <div>
-            <Header/>
+            <Header />
+            <div className="home-state center">
+                {user ? (
+                    <>
+                        <img src={user.photoURL} alt="Profile" className="profile-picture-home" />
+                        <h1>👋Hi, <span>{user.displayName}</span></h1>
+                        {/* <h2>{user.email}</h2> */}
+                    </>
+                ) : (
+                    <>
+                        <h2>Hi, Guest</h2>
+                        <p>Please sign in to view your profile.</p>
+                    </>
+                )}
+                <div>
+                    <h2>
+                        <Link to="/start-building" className="google-button">Start Building</Link>
+                    </h2>
+                    <h2>
+                        <Link to="/about" className="google-button">About</Link>
+                    </h2>
+                    <h2>
+                        <Link to="/profile" className="google-button">Profile</Link>
+                    </h2>
+                </div>
+            </div>
         </div>
     );
 }
